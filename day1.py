22292c77b6552b8e236cb678d868fe0a0446d3fc
Ant Bnt <bobs9842072150@gmail.com>
2024-02-19 18:33:54 +0545

    First Commit

---
+++ day1.py
@@ -0,0 +1,15 @@
+# Learn python from scratch
+# So the first program is simple printing
+
+print("Hello Learners")
+#this will give you an output Hello Learner
+#print is simply a command to display anything
+# " " is used to initiate strings
+#string are simply alpha-numeric data
+#alpha-numeric data are combination of alphabets or numbers
+
+print(5+6)#Results 11
+print(99-45)#Results 54 
+print(66*2)#Results  132
+print(48/2)#Results  24.0
+
